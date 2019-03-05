26f6b8ac0efa7dd6300b708a2074fda99aacf2e7
shanthivimalanataraajan01 <46440743+shanthivimalanataraajan01@users.noreply.github.com>
2019-03-05 09:48:33 +0530

    Update between_l_and_r.py

--- between_l_and_r.py
+++ between_l_and_r.py
@@ -1,12 +1,12 @@
-# your code goes here
+#ms
 n=int(input())
-c=0
+d=0
 a,b=map(int,input().split())
 for i in range(a+1,b):
 	if i==n:
-		c+=1
+		d+=1
 		break
-if c>0:
+if d>0:
 	print('yes')
 else:
 	print('no')
